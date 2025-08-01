52637a919d851ea7d6fd1278e00c211eece9b518
Andrew Kornilov <akornilov@gmail.com>
2025-08-01 03:30:26 +0300

    Fail the whole container if one process fails

--- usr/local/bin/entrypoint.sh
+++ usr/local/bin/entrypoint.sh
@@ -68,6 +68,10 @@ if [ -n "${SOCKS5H_HOST:-}" ]; then
     echo "stdout_logfile_maxbytes=0"
     echo "stderr_logfile=/dev/stderr"
     echo "stderr_logfile_maxbytes=0"
+    echo ""
+    echo "[eventlistener:processes]"
+    echo "command=bash -c \"printf 'READY\n' && while read line; do kill -SIGQUIT $PPID; done < /dev/stdin\""
+    echo "events=PROCESS_STATE_STOPPED,PROCESS_STATE_EXITED,PROCESS_STATE_FATAL"
   } >/etc/supervisord.conf
 
   exec /usr/bin/supervisord -c /etc/supervisord.conf
